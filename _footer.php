91eada76771780da718a4a119abfa17efffa6003
DanielRuf <daniel@daniel-ruf.de>
2016-04-25 20:16:56 +0200

    included scripts and styles, fixed links and copyright notice, removed unused code and some other changes

--- _footer.php
+++ _footer.php
@@ -1,23 +1,22 @@
 <!-- Footer -->
-<footer>
-  <div class="container">
-      <div class="copy">
-        <p class="author">
-            PHP Apache2 Basic Auth Manager © 2014 <span><a href="http://tech.rgou.net">Rafael Goulart</a></span> 
-            | Licensed under <a href="license.php">GPL3</a> 
-            | <a href="https://github.com/rafaelgou/php-apache2-basic-auth-manager">Fork on <i class="fa fa-github"></i> Github</a>
-        </p>
+  <footer>
+    <div class="container">
+        <div class="copy">
+          <p class="author">
+              PHP Apache2 Basic Auth Manager &copy; <?php echo date("Y"); ?> <span><a target="_blank" href="http://tech.rgou.net">Rafael Goulart</a>, <a target="_blank" href="https://daniel-ruf.de">Daniel Ruf</a></span> 
+              | Licensed under <a href="license.php">GPL3</a> 
+              | <a target="_blank" href="https://github.com/rafaelgou/php-apache2-basic-auth-manager">Fork on <i class="fa fa-github"></i> Github</a>
+          </p>
+        </div>
       </div>
+      <div class="clearfix"></div>
     </div>
-    <div class="clearfix"></div>
-  </div>
-</footer>
+  </footer>
 
-<!-- Javascript CDN -->
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.1.0/bootbox.min.js"></script>
-<script type="text/javascript" src="script.js"></script>
+  <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/bootbox.min.js"></script>
+  <script type="text/javascript" src="script.js"></script>
 
   </body>
 </html>
